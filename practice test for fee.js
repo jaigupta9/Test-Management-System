585cecf463f13ef31484cc2179d4dfcf46dffeb8
Nitesh Kushwaha <157011694+2310992001@users.noreply.github.com>
2024-05-14 20:29:11 +0530

    Add files via upload

---
+++ practice test for fee.js
@@ -0,0 +1,54 @@
+const startBtn=document.querySelector('.start-btn');
+const popupinfo= document.querySelector('.popup-info');
+const exitbtn= document.querySelector('.exit-btn');
+
+startBtn.onclick= ()=>{
+    popupinfo.classList.add('active');
+}
+exitbtn.onclick=() => {
+    popupinfo.classList.remove('active');
+}
+
+const startBtn2=document.querySelector('.start-btn2');
+const popupinfo2= document.querySelector('.popup-info2');
+const exitbtn2= document.querySelector('.exit-btn2');
+
+startBtn2.onclick= ()=>{
+    popupinfo2.classList.add('active');
+}
+exitbtn2.onclick=() => {
+    popupinfo2.classList.remove('active');
+}
+
+const startBtn3=document.querySelector('.start-btn3');
+const popupinfo3= document.querySelector('.popup-info3');
+const exitbtn3= document.querySelector('.exit-btn3');
+
+startBtn3.onclick= ()=>{
+    popupinfo3.classList.add('active');
+}
+exitbtn3.onclick=() => {
+    popupinfo3.classList.remove('active');
+}
+
+const startBtn4=document.querySelector('.start-btn4');
+const popupinfo4= document.querySelector('.popup-info4');
+const exitbtn4= document.querySelector('.exit-btn4');
+
+startBtn4.onclick= ()=>{
+    popupinfo4.classList.add('active');
+}
+exitbtn4.onclick=() => {
+    popupinfo4.classList.remove('active');
+}
+
+const startBtn5=document.querySelector('.start-btn5');
+const popupinfo5= document.querySelector('.popup-info5');
+const exitbtn5= document.querySelector('.exit-btn5');
+
+startBtn5.onclick= ()=>{
+    popupinfo5.classList.add('active');
+}
+exitbtn5.onclick=() => {
+    popupinfo5.classList.remove('active');
+}
